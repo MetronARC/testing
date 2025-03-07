8b8d8e120415ad5c3df34a544db356104c962e0a
MetronARC <ricoroenaldo@metronarc.com>
2025-03-07 09:02:03 +0700

    first push

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,9 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
-
-        <h2>The small framework with powerful features</h2>
+        <h1>This is MetronARC Template for CodeIgniter 4</h1>
 
     </div>
 
